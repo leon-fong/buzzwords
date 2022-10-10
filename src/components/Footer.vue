91f8a84d2a763b7cad74c68cbacf890e09f6f3e8
leon-fong <ooohmydawn@hotmail.com>
2022-10-10 22:24:00 +0800

    style : footer UI update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,19 @@
 <template>
   <nav text-xl mt-6 inline-flex gap-2>
-    Made by<a
-      rel="noreferrer"
-      href="https://leonfong.me"
-      target="_blank"
-      title="leonfong.me"
-      class="text-sm"
-    > Leon</a>
+    <span class="text-sm">
+      Made by<a
+        rel="noreferrer"
+        href="https://leonfong.me"
+        target="_blank"
+        title="leonfong.me"
+        class="font-bold"
+      > Leon</a>
+    </span>
 
     <a
       icon-btn i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="https://github.com/leon-fong/buzzwords"
       target="_blank"
       title="GitHub"
     />
